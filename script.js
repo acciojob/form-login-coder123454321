d29dbbaddcc826dfe4a64fcf498bf347398d718e
coder123454321 <115930973+coder123454321@users.noreply.github.com>
2026-09-22 01:17:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const first_name=document.getElementsByName("fname");
+const Last_Name=document.getElementsByName("lname");
 function getFormvalue() {
     //Write your code here
+	const f_name=first_name[0].value;
+	const l_name=Last_Name[0].value;
+	 alert(
+        f_name + " " + l_name
+		 )
 
 }
